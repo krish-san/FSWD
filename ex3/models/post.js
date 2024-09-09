b99ac35912b2d3de1e9a11800505212a690850f0
san <120664435+krish-san@users.noreply.github.com>
2024-09-09 22:27:19 +0530

    Add files via upload

---
+++ ex3/models/post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    content: { type: String, required: true, maxlength: 280 },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
---
+++ ex3/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
+
+// Hash password before saving user
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Compare hashed password for login
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
